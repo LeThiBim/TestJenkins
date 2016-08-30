1c3524ca4380f2372352eb5225fc31565e406360
huynq <lethibim@gmail.com>
2016-08-30 23:03:19 +0700

    - Test Jenkins build

--- TestSwift/ViewController.swift
+++ TestSwift/ViewController.swift
@@ -64,39 +64,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    func detectKindOfArray(inout A : [Int]) -> String
-    {
-        // if array is sum of (even slice + odd number) or (odd number + even slice), you can win by removing easily even slice
-        
-        // This is the input string.
-        let s = "one two three"
-        
-        // Get range 4 places from the start, and 6 from the end.
-        let r = s.startIndex.advancedBy(4)..<s.endIndex.advancedBy(-6)
-        
-        // Access the string by the range.
-        let substring = s[r]
-        print(substring)
-        return "-1"
-        
-        
-    
-    }
-    
-    func stairCase(number: Int)
-    {
-        for i in 1...number
-        {
-            var string1: String = ""
-            
-            for _ in 1...i
-            {
-                string1 = string1 + "#"
-            }
-            
-            print(string1)
-        }
-    }
 
 }
 
