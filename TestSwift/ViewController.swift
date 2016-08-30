6e6c517c8f9d4d51e565501c59bc983ba8008597
huynq <lethibim@gmail.com>
2016-08-30 23:01:25 +0700

    - Test Jenkins build

--- TestSwift/ViewController.swift
+++ TestSwift/ViewController.swift
@@ -36,26 +36,26 @@ class ViewController: UIViewController {
         
         
         
-        let digitNames = [
-            0: "Zero", 1: "One", 2: "Two",   3: "Three", 4: "Four",
-            5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"
-        ]
-        let numbers = [16, 58, 510]
+//        let digitNames = [
+//            0: "Zero", 1: "One", 2: "Two",   3: "Three", 4: "Four",
+//            5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"
+//        ]
+//        let numbers = [16, 58, 510]
 
         
         
-        let strings = numbers.map {
-            (number) -> String in
-            var number = number
-            var output = ""
-            while number > 0 {
-                output = digitNames[number % 10]! + output
-                number /= 10
-            }
-            return output
-        }
-        
-        print("RESULT : \(strings)")
+//        let strings = numbers.map {
+//            (number) -> String in
+//            var number = number
+//            var output = ""
+//            while number > 0 {
+//                output = digitNames[number % 10]! + output
+//                number /= 10
+//            }
+//            return output
+//        }
+//        
+//        print("RESULT : \(strings)")
         
     }
 
